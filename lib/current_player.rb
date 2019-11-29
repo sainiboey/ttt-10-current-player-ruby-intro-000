4f61a666b7edea4f2155070a8ee91941399f3bc4
sainiboey <kabising@gmail.com>
2019-11-29 22:11:12 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,7 +1,7 @@
 def turn_count(board)
   counter=0
   board.each do |num|
-    if num != "" || num != " " || num != nil
+    if (num != "" || num != " " || num != nil)
       counter += 1
     end
   end
