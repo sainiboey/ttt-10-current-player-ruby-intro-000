0efef885e5aac9634f931dced9cd690f98c3bf67
sainiboey <kabising@gmail.com>
2019-11-29 22:08:53 +0000

    Automatically backed up by Learn

---
+++ lib/current_player.rb
@@ -0,0 +1,14 @@
+def turn_count(board)
+  counter=0
+  board.each do |num|
+    if num != "" || num != " " || num != nil
+      counter += 1
+    end
+  end
+  counter
+end
+
+def current_player(board)
+  num_played = turn_count(board)
+  num_played % 2?"X":"O"
+end
